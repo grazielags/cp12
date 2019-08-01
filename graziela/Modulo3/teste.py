d775279e5d17d8cc2480894a3658c520e2822dd1
Graziela Göedert de Souza <graziela@gmail.com>
2019-08-01 18:31:56 -0300

    Módulo.

---
+++ graziela/Modulo3/teste.py
@@ -0,0 +1,14 @@
+nome=input("Digite o nome: ")
+print (nome)
+
+#print ("Nova linha")
+'''
+print ("Teste")
+print ("Novo teste")
+'''
+"""
+print ("Teste")
+print ("Novo teste")
+"""
+
+print("{:.2f}".format(1/3))
