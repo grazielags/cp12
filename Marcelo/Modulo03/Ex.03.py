43f79a0556490a809d81240acc1311bc6869d191
Marcelo Walther <marcelowdsilva@gmail.com>
2019-08-06 16:47:36 -0300

    Exercicios python

--- Marcelo/Modulo03/Ex.03.py
+++ Marcelo/Modulo03/Ex.03.py
@@ -8,5 +8,5 @@ numero4= int(input("Escreva nota"))
 print (numero4)
 numero5= int(input("Escreva nota"))
 print (numero5)
-media = numero1//numero2//numero3//numero4//numero5
+media = (numero1+numero2+numero3+numero4+numero5)//5
 print ("A media das notas é :",media)
