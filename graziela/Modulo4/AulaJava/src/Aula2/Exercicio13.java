03f45759c4bab5b17f73e088f7e633fc453bccbb
Graziela Göedert de Souza <graziela@gmail.com>
2019-08-29 19:03:01 -0300

    Exercicios aula 2.

---
+++ graziela/Modulo4/AulaJava/src/Aula2/Exercicio13.java
@@ -0,0 +1,16 @@
+package Aula2;
+
+public class Exercicio13 {
+	public static void main(String[] args) {
+		/*
+		 * Crie uma calculadora usando a instrução SWITCH,
+		 * que pergunte qual das operações básicas quer
+		 * fazer (+, -, * e /), em seguida peça os dois números
+		 * e mostre o resultado da operação matemática entre eles.
+		 * Faça um programa que obtenha um número de 1 até 12 e
+		 * retorne o mês e a quantidade de dias que esse número
+		 * representa. Lembre-se dos anos bissextos
+		 * (Fevereiro possui 29 dias).
+		 */
+	}
+}
