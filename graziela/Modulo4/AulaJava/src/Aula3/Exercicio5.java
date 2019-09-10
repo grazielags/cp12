5c30a2e835519320ca585f56156ffbccb2fbeba6
Graziela Göedert de Souza <graziela@gmail.com>
2019-09-10 19:29:51 -0300

    Exercícios da Aula 3.

--- graziela/Modulo4/AulaJava/src/Aula3/Exercicio5.java
+++ graziela/Modulo4/AulaJava/src/Aula3/Exercicio5.java
@@ -1,10 +1,24 @@
 package Aula3;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio5 {
 	public static void main(String args[]) {
 		/*
 		 * Escreva um algoritmo que calcule a média dos números digitados pelo usuário, se eles forem pares.
 		 * Termine a leitura se o usuário digitar zero (0).
 		 */
+		int numero = -1;
+		int quantidade = 0;
+		int somaNumeros = 0;
+		while(numero != 0) {
+			numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número!"));
+			if (numero != 0 && numero % 2 == 0) {
+				somaNumeros += numero;
+				quantidade++;
+			}
+		}
+		double media = somaNumeros / (double) quantidade;
+		JOptionPane.showMessageDialog(null, "A média dos números pares digitados é: " + media);
 	}
 }
--- graziela/Modulo4/AulaJava/src/Aula3/Exercicio6.java
+++ graziela/Modulo4/AulaJava/src/Aula3/Exercicio6.java
@@ -1,10 +1,21 @@
 package Aula3;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio6 {
 	public static void main(String[] args) {
 		/*
 		 * Escrever um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir.
 		 * Para cada número lido, mostre uma tabela contendo o valor lido e o fatorial deste valor.
 		 */
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de valores que deseja ler: "));
+		for (int i = 1; i <= n; i++) {
+			int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite o número que deseja saber o fatorial: "));
+			int fatorial = 1;
+			for (int numeroFatorial = numero; numeroFatorial > 0 ; numeroFatorial--) {
+				fatorial = fatorial * numeroFatorial;
+			}
+			JOptionPane.showMessageDialog(null, "A fatorial de " + numero + " é: " + fatorial);
+		}
 	}
 }
